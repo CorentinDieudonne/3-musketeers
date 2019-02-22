1e21e02ae37f9a9d5ec5cf82c5e93e052a86eba5
CorentinDieudonne <corentin.dieudonne@gmail.com>
2019-02-22 23:02:26 +0100

    docs(readme): finish the jsdoc

--- cash/bin/cash.js
+++ cash/bin/cash.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/** The list of the requirement to use that program */
 const got = require('got');
 const money = require('money');
 const chalk = require('chalk');
@@ -7,12 +7,12 @@ const ora = require('ora');
 const currencies = require('../lib/currencies.json');
 
 const {API} = require('./constants');
-
+/** This is an async command due to the access to the API, to verify that the character is an upper  */
 const cash = async command => {
 	const {amount} = command;
 	const from = command.from.toUpperCase();
 	const to = command.to.filter(item => item !== from).map(item => item.toUpperCase());
-
+	/** Writing the information with a green color and make the currency spins before showing the result */
 	console.log();
 	const loading = ora({
 		text: 'Converting...',
@@ -24,7 +24,12 @@ const cash = async command => {
 	});
 
 	loading.start();
-
+	/** 
+	*Use an await to have it before showing the answer and linked with the waiting timer written before*/
+	* Verify the value in the API
+	* Get the value with the rates of the new currencies when the json match with value typed.
+	* If the program works well, show a little green check, else we got a yellow and show that the currency don't match with the json 
+	*/
 	await got(API, {
 		json: true
 	}).then(response => {
@@ -38,7 +43,10 @@ const cash = async command => {
 				loading.warn(`${chalk.yellow(`The "${item}" currency not found `)}`);
 			}
 		});
-
+	/**
+	*Show the answer
+	*If there is internet connection problem show a fail message
+	*/
 		console.log(chalk.underline.gray(`\nConversion of ${chalk.bold(from)} ${chalk.bold(amount)}`));
 	}).catch(error => {
 		if (error.code === 'ENOTFOUND') {
